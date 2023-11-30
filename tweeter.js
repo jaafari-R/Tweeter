761d528a44fbd668e4a4715fd433e8997c942fbe
jaafari-R <jaafariramadan@gmail.com>
2023-11-30 14:58:04 +0200

    Added Initial Tweeter model values

--- tweeter.js
+++ tweeter.js
@@ -1,36 +1,45 @@
+const COMMENT_ID_PREFIX = 'c';
+const POST_ID_PREFIX = 'p';
+const INIT_POST_ID_COUNTER = 0;
+const INIT_COMMENT_ID_COUNTER = 0;
+
 const Comment = function(id, text) {
     return {
-        id: "c"+id,
+        id: COMMENT_ID_PREFIX + id,
         text
     }
 }
 
 const Post = function(id, text) {
     return {
-        id: 'p'+id,
+        id: POST_ID_PREFIX + id,
         text,
         comments: []
     }
 }
 
-const Tweeter = function() {
-    const _posts = [];
-    let postIdCounter = 0;
-    let commentIdCounter = 0;
+const Tweeter = function(posts, initPostIdCounter, initCommentIdCounter) {
+    const _posts = JSON.parse(JSON.stringify(posts)) || [];
+    let postIdCounter;
+    let commentIdCounter;
+    
+    initIdCounters();
+    console.log(postIdCounter);
+    console.log(commentIdCounter);
 
     const getPosts = () => {
         return _posts;
-    }
+    };
 
     const addPost = (text) => {
         const postId = ++postIdCounter;
         _posts.push(Post(postId, text));
-    }
+    };
 
     const removePost = (id) => {
         const postIndex = getItemIndexById(_posts, id);
         _posts.splice(postIndex, 1);
-    }
+    };
 
     const addComment = (text, postId) => {
         const postIndex = getItemIndexById(_posts, postId);
@@ -39,18 +48,49 @@ const Tweeter = function() {
         console.log(postIndex, postId)
 
         _posts[postIndex].comments.push(newComment);
-    }
+    };
 
     const removeComment = (postId, commentId) => {
         const postIndex = getItemIndexById(_posts, postId);
         const commentIndex = getItemIndexById(_posts[postIndex].comments, commentId);
         _posts[postIndex].comments.splice(commentIndex, 1);
-    }
+    };
+
 
     // ------ Helpers ------ \\
     const getItemIndexById = (arr, id) => {
         return arr.findIndex(item => item.id === id);
-    }
+    };
+
+
+    // ------ initializers ------ \\
+    function initIdCounters() {
+        postIdCounter = initPostIdCounter || INIT_POST_ID_COUNTER;
+        commentIdCounter = initCommentIdCounter || INIT_COMMENT_ID_COUNTER;
+
+        if(_posts && postIdCounter == INIT_POST_ID_COUNTER) {
+            calcPostIdCounter();
+        }
+        if(_posts && commentIdCounter == INIT_POST_ID_COUNTER) {
+            calcCommentIdCounter();
+        }
+    };
+
+    function calcPostIdCounter() {
+        _posts.forEach((post) => {
+            postIdCounter = Math.max(postIdCounter, post.id.replace(POST_ID_PREFIX, ""));
+        });
+    };
+    
+    function calcCommentIdCounter() {
+        _posts.forEach((post) => {
+            post.comments.forEach((comment) => {
+                commentIdCounter = Math.max(commentIdCounter, comment.id.replace(COMMENT_ID_PREFIX, ""));
+            });
+        });
+    };
+    
+
 
     return {
         getPosts,
@@ -58,5 +98,5 @@ const Tweeter = function() {
         removePost,
         addComment,
         removeComment
-    }
+    };
 }
